777cc82eb3ac03ab2d2d25eb4d499e0ddbe2c689
jmpeoples <jmpeoples.88@gmail.com>
2022-09-19 10:35:14 -0500

    setup mixpanel

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,13 @@
 import Head from 'next/head'
 import { Hero } from '@/components/Hero'
+import mixpanel from 'mixpanel-browser';
 
 export default function Home() {
+  mixpanel.init('acc2963ea857676b29b8a0a67802a22f', {debug: true}); 
+  mixpanel.track('Sign Up', {
+    'source': "Pat's affiliate site",
+    'Opted out of email': true,
+  });
   return (
     <>
     <Head>
